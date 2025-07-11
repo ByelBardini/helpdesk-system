c8331546951c484eeb8488518125b96444afeb43
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-07-11 13:16:52 -0300

    feat: adicionado função de rodar o backend e conexão com MySQL

---
+++ server/config/db.js
@@ -0,0 +1,22 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  port: process.env.PORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: "chamados_ti",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar ao MySQL: ", err);
+    return;
+  }
+  console.log("Conexão com MySQL estabelecida com sucesso!");
+});
+
+export default connection;
