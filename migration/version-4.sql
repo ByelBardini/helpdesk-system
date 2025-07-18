d88e74a5d89dd05facf4118ac472c2122f985597
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-07-18 17:14:52 -0300

    refactor: adicionado ids de status básicos para funcionamento do código

---
+++ migration/version-4.sql
@@ -0,0 +1,6 @@
+INSERT INTO status(status_id, status_nome, status_tipo) 
+VALUES (1, 'Novo', 'chamado'), 
+       (2, 'Visualizado', 'chamado'), 
+       (3, 'Em Andamento', 'chamado'), 
+       (4, 'Agendado', 'chamado'), 
+       (5, 'Finalizado', 'chamado');
