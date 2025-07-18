8bc0c3d19067298643b4a380208bf53393813e30
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-07-18 18:05:44 -0300

    refactor: alterado valores de status

--- migration/version-4.sql
+++ migration/version-4.sql
@@ -4,3 +4,7 @@ VALUES (1, 'Novo', 'chamado'),
        (3, 'Em Andamento', 'chamado'), 
        (4, 'Agendado', 'chamado'), 
        (5, 'Finalizado', 'chamado');
+
+INSERT INTO status (status_id, status_nome, status_tipo)
+VALUES (6, 'Agendado', 'agendamento'),
+	   (7, 'Concluido', 'agendamento');
