992d3dc8e734ef42101e265ac4245ed0819e7549
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-09-22 17:20:35 -0300

    refactor: adiciona página FAQ no router do app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Login from "./pages/Login.jsx";
 import Home from "./pages/Home.jsx";
+import Faq from "./pages/Faq.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/home",
     element: <Home />,
+  },
+  {
+    path: "/faq",
+    element: <Faq />,
   },
 ]);
 
