4e94dbc0dec00e8bc94031da1cdbf05e839c73e3
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-09-25 09:37:12 -0300

    feat: implementado função para puxar os chamados do endpoint

--- client/src/services/api/chamadosServices.js
+++ client/src/services/api/chamadosServices.js
@@ -1,5 +1,16 @@
 import { api } from "../api.js";
 
+export async function getChamadosUsuario(id) {
+  try {
+    const response = await api.get(`/chamado/usuario/${id}`);
+
+    return response.data;
+  } catch (err) {
+    console.error("Erro em getChamadosUsuario:", err);
+    throw err;
+  }
+}
+
 export async function postChamado(fd) {
   try {
     const response = await api.post("/chamado", fd, {
