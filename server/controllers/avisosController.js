c2cd065f40ab095d87df63ed4b16ddf853f8996e
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-10-02 14:11:46 -0300

    feat: adiciona endpoint para excluir avisos

--- server/controllers/avisosController.js
+++ server/controllers/avisosController.js
@@ -6,3 +6,15 @@ export async function getAvisos(req, res) {
 
   return res.status(200).json(avisos);
 }
+
+export async function deleteAviso(req, res) {
+  const { id } = req.params;
+  if (!id) {
+    throw ApiError.badRequest("Id do aviso é obrigatório");
+  }
+
+  const aviso = await Aviso.findByPk(id);
+  await aviso.destroy();
+
+  return res.status(200).json({ message: "Aviso excluído com sucesso" });
+}
--- server/routes/avisosRoutes.js
+++ server/routes/avisosRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAvisos } from "../controllers/avisosController.js";
+import { getAvisos, deleteAviso } from "../controllers/avisosController.js";
 
 const router = express.Router();
 
 router.get("/", getAvisos);
+router.delete("/:id", deleteAviso);
 
 export default router;
