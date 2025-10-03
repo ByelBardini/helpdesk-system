41c2f3007966c37757d09e8a05dba3695ed1161f
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-10-03 15:07:50 -0300

    feat: adiciona endpoint para editar perguntas

--- server/controllers/faqController.js
+++ server/controllers/faqController.js
@@ -19,3 +19,24 @@ export async function deletePergunta(req, res) {
 
   return res.status(200).json({ message: "Pergunta deletada com sucesso" });
 }
+
+export async function putPergunta(req, res) {
+  const { id } = req.params;
+  if (!id) {
+    throw ApiError.badRequest("Id da pergunta é obrigatório");
+  }
+  const { categoria, titulo, resposta } = req.body;
+  if (!categoria || !titulo || !resposta) {
+    throw ApiError.badRequest("Todos os dados são obrigatórios");
+  }
+
+  const pergunta = await Pergunta.findByPk(id);
+
+  pergunta.pergunta_categoria = categoria;
+  pergunta.pergunta_titulo = titulo;
+  pergunta.pergunta_resposta = resposta;
+
+  await pergunta.save();
+
+  return res.status(200).json({ message: "Pergunta editada com sucesso" });
+}
--- server/routes/faqRoutes.js
+++ server/routes/faqRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { getPerguntas, deletePergunta } from "../controllers/faqController.js";
+import {
+  getPerguntas,
+  deletePergunta,
+  putPergunta,
+} from "../controllers/faqController.js";
 import { autenticar, autorizarRoles } from "../middlewares/autenticaToken.js";
 
 const router = express.Router();
 router.use(autenticar);
 
 router.get("/", getPerguntas);
+router.put("/:id", autorizarRoles("adm"), putPergunta);
 router.delete("/:id", autorizarRoles("adm"), deletePergunta);
 
 export default router;
